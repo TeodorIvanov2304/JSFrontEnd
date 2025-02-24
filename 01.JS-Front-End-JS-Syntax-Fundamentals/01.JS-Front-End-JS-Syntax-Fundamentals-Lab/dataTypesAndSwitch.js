15d7b9d8acbd0c579d908469292ffecefaa88cf9
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-02-24 16:05:51 +0200

    Create dataTypesAndSwitch.js

---
+++ 01.JS-Front-End-JS-Syntax-Fundamentals/01.JS-Front-End-JS-Syntax-Fundamentals-Lab/dataTypesAndSwitch.js
@@ -0,0 +1,24 @@
+let number = 10; // Number
+let person = { name: "George", age: 24 }; //Object
+let array = [1, 2, 3, 4, 5]; //Array
+let isTrue = true; //Boolean
+let name = "Georg"; //String
+let empty = null; //Null
+let unknown = undefined; //Undefined
+
+//Switch-case
+
+let myVariable = 'hello';
+
+switch (myVariable) {
+  case 'hello':
+    console.log("Da");
+    break;
+  case 'helloo':
+    console.log("Ne");
+    break;
+  default:
+    console.log("Default");
+    break;
+}
+
