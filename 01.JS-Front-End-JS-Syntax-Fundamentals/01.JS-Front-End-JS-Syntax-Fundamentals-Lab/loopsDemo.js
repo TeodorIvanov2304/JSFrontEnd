e4d982207a87f01fc27e8e749d5f125452233948
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-02-24 20:41:26 +0200

    Create loopsDemo.js

---
+++ 01.JS-Front-End-JS-Syntax-Fundamentals/01.JS-Front-End-JS-Syntax-Fundamentals-Lab/loopsDemo.js
@@ -0,0 +1,16 @@
+//For-loop
+
+for(let i = 0;i < 5; i++) {
+    console.log(i);
+}
+
+console.log('------------------------------------------')
+
+//While-loop
+
+let i = 1;
+
+while(i <= 5) {
+    console.log(i);
+    i++;
+}
