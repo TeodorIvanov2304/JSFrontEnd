a75665744a80c3fdb207500b33a2c27d6c11cda7
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-02-24 16:44:58 +0200

    Create typeOfDemo.js

---
+++ 01.JS-Front-End-JS-Syntax-Fundamentals/01.JS-Front-End-JS-Syntax-Fundamentals-Lab/typeOfDemo.js
@@ -0,0 +1,12 @@
+//typeof returns string!
+
+console.log(typeof 5);
+console.log(typeof 'hello');
+console.log(typeof [1,2,3]); //Array is also object
+console.log(typeof true);
+console.log(typeof 15n);
+console.log(typeof null); //null returns also object
+console.log(typeof undefined);
+console.log(typeof {name: 'George',age:1 });
+console.log(typeof Symbol('goodbye'));
+console.log(typeof NaN);
