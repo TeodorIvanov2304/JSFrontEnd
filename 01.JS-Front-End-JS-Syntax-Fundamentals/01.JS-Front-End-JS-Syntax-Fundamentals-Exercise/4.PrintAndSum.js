c8a151bdaa77463d533ae7d20dab6ea666d24e63
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-02-28 10:43:44 +0200

    Update 4.PrintAndSum.js

--- 01.JS-Front-End-JS-Syntax-Fundamentals/01.JS-Front-End-JS-Syntax-Fundamentals-Exercise/4.PrintAndSum.js
+++ 01.JS-Front-End-JS-Syntax-Fundamentals/01.JS-Front-End-JS-Syntax-Fundamentals-Exercise/4.PrintAndSum.js
@@ -17,3 +17,26 @@ function printAndSum (start, end) {
 printAndSum(5,10);
 printAndSum(0,26);
 printAndSum(50,60);
+
+console.log('---------------------------------------')
+
+function printAndSumString (start, end) {
+
+    let sum = 0;
+    let numbers = '';
+
+    for (let i = start; i <= end; i++) {
+        
+        numbers += i;
+        numbers += ' ';
+
+        sum += i;
+    }
+
+    console.log(numbers);
+    console.log(`Sum: ${sum}`);
+}
+
+printAndSumString(5,10);
+printAndSumString(0,26);
+printAndSumString(50,60);
