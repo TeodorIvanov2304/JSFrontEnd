d61e402df0fe5354a36f467e9728b0d5673db7a8
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-03-07 21:10:00 +0200

    Update 2.PrintEveryN-thElementfromAnArray.js
    Add second solution with filter

--- 02.JS-Front-End-JS-Arrays-and-Strings/02.JS-Front-End-JS-Arrays-and-Strings-Exercise/2.PrintEveryN-thElementfromAnArray.js
+++ 02.JS-Front-End-JS-Arrays-and-Strings/02.JS-Front-End-JS-Arrays-and-Strings-Exercise/2.PrintEveryN-thElementfromAnArray.js
@@ -9,3 +9,14 @@ function solve(arr,step) {
 solve(["5", "20", "31", "4", "20"], 2);
 solve(["dsa", "asd", "test", "tset"], 2);
 solve(["1", "2", "3", "4", "5"], 6);
+
+
+//Second solution, 2x slower
+function solve2(arr,step) {
+    //filter takes two parameters, element(e) an index(i)
+    return arr.filter((e, i) => i % step == 0);
+}
+
+solve2(["5", "20", "31", "4", "20"], 2);
+solve2(["dsa", "asd", "test", "tset"], 2);
+solve2(["1", "2", "3", "4", "5"], 6);
