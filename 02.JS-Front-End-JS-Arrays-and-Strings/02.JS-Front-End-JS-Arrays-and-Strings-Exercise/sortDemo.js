3fb864969ca4029a954f777e1386bda5aabf6acf
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-03-08 09:54:56 +0200

    Update sortDemo.js

--- 02.JS-Front-End-JS-Arrays-and-Strings/02.JS-Front-End-JS-Arrays-and-Strings-Exercise/sortDemo.js
+++ 02.JS-Front-End-JS-Arrays-and-Strings/02.JS-Front-End-JS-Arrays-and-Strings-Exercise/sortDemo.js
@@ -3,3 +3,12 @@ console.log(arr);
 //a - b for ascending order, b - a for descending
 arr.sort((a , b) => a - b);
 console.log(arr);
+
+
+//Order strings aplphabetically with localeCompare(a)
+let stringArray = ['Michael', 'An-Marie', 'David', 'Klara','Johnny','Ted'];
+stringArray.sort((a,b) => a.localeCompare(b));
+console.log(stringArray);
+//From z to a
+stringArray.sort((a,b) => b.localeCompare(a));
+console.log(stringArray);
