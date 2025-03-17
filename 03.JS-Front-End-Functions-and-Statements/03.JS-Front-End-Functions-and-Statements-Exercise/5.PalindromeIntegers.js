f9646fdc64c1e7000e5099a0ee520e54a4a90bf3
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-03-17 10:54:13 +0200

    Create 5.PalindromeIntegers.js

---
+++ 03.JS-Front-End-Functions-and-Statements/03.JS-Front-End-Functions-and-Statements-Exercise/5.PalindromeIntegers.js
@@ -0,0 +1,38 @@
+function palindromeIntegers(arrOfIntegers) {
+    for (let element of arrOfIntegers) {
+
+        //Cast the element to string and reverse it
+        let reversedElement = element.toString().split('').reverse().join('');
+        
+        //Compare element with reversed element
+        if(element == reversedElement){
+            console.log('true');
+        }
+        else {
+            console.log('false');
+        }
+    }
+}
+
+palindromeIntegers([123,323,421,121]);
+palindromeIntegers([32,2,232,1010]);
+
+console.log('*****************');
+
+function palindromeIntegers2(arrOfIntegers) {
+    for (let integer of arrOfIntegers) {
+        let integerAsString = integer.toString();
+        let isPalindrome = true;
+
+        for (let i = 0; i < (integerAsString.length -1) / 2; i++) {
+            if(integerAsString[i] != integerAsString[integerAsString.length - i - 1]) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        console.log(isPalindrome);
+    }
+}
+
+palindromeIntegers2([123,323,421,121]);
+palindromeIntegers2([32,2,232,1010]);
