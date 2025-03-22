0b192d5bd5025f80dc64da4ec7237031ba58175a
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-03-23 00:31:11 +0200

    Update 5.PhoneBook.js
    Add two more solutions

--- 04.JS-Front-end-Objects-and-Classes/04.JS-Front-end-Objects-and-Classes-Lab/5.PhoneBook.js
+++ 04.JS-Front-end-Objects-and-Classes/04.JS-Front-end-Objects-and-Classes-Lab/5.PhoneBook.js
@@ -1,14 +1,14 @@
 function phoneBook(arr) {
-    let personMap = new Map();
+    let phonebook = new Map();
 
     for(let entry of arr) {
         let splittedEntry = entry.split(' ');
         let name = splittedEntry[0];
         let phoneNumber = splittedEntry[1];
-        personMap[name] = phoneNumber;
+        phonebook[name] = phoneNumber;
     }
 
-    let entries = Object.entries(personMap);
+    let entries = Object.entries(phonebook);
     for(let entry of entries) {
         console.log(`${entry[0]} -> ${entry[1]}`);
     }
@@ -28,3 +28,60 @@ phoneBook([
     "Bill 0845344"
 ]);
 
+console.log('************************');
+
+//With for-in
+function phoneBook2(data) {
+    let phonebook = {};
+
+    for(let item of data) {
+        let [name, phone] = item.split(' ');
+        phonebook[name] = phone;
+    }
+    
+    for (const name in phonebook) {
+        console.log(`${name} -> ${phonebook[name]}`)
+    }
+}
+
+phoneBook2([
+  "Tim 0834212554",
+  "Peter 0877547887",
+  "Bill 0896543112",
+  "Tim 0876566344",
+]);
+
+phoneBook2([
+    "George 0552554", 
+    "Peter 087587", 
+    "George 0453112", 
+    "Bill 0845344"
+]);
+
+console.log('************************');
+
+//With fromEntries
+function phoneBook3(data) {
+    
+    let entries  = data.map(i => i.split(' '));
+
+    let phonebook = Object.fromEntries(entries);
+    
+    for (const name in phonebook) {
+        console.log(`${name} -> ${phonebook[name]}`)
+    }
+}
+
+phoneBook3([
+  "Tim 0834212554",
+  "Peter 0877547887",
+  "Bill 0896543112",
+  "Tim 0876566344",
+]);
+
+phoneBook3([
+    "George 0552554", 
+    "Peter 087587", 
+    "George 0453112", 
+    "Bill 0845344"
+]);
