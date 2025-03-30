27bf09eade394afc43e9cdc4fb745e7c636c2385
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-03-30 16:26:21 +0300

    Create sum-table.js

---
+++ 05.JS-Front-End-DOM-Introduction/05.JS-Front-End-DOM-Introduction-Lab/05.JS-Front-End-DOM-Introduction-Lab-Resources/06.Sum Table/sum-table.js
@@ -0,0 +1,16 @@
+function sumTable() {
+    // TODO
+    let rows = Array.from(document.querySelectorAll('table tr'));
+    rows.shift();
+    let output = rows.pop();
+
+    let sum = 0;
+
+    for(let row of rows){
+        let col = row.children[row.children.length - 1];
+        sum += Number(col.textContent);
+    }
+
+    let outputCol = output.children[output.children.length - 1];
+    outputCol.textContent = sum
+}
