32b4f46caa9e6e129402943837b6df24d3ce431e
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-04-04 15:12:19 +0300

    Create shopping-cart.js

---
+++ 06.JS-Front-End-DOM-Events/06.JS-Front-End-DOM-Events-Lab/06.JS-Front-End-DOM-Events-Lab-Resources/07. Shopping-Cart/shopping-cart.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', solve);
+
+function solve() {
+   let buyBtns = document.querySelectorAll('.add-product');
+   let output = document.querySelector('textarea');
+   output.value = '';
+
+   let cart = {};
+
+   for (let btn of buyBtns) {
+      btn.addEventListener('click', onBuyClick);
+   }
+
+   let checkOutBtn = document.querySelector('.checkout');
+   checkOutBtn.addEventListener('click', onCheckout);
+
+   function onBuyClick(event) {
+      let productRow = event.target.parentElement.parentElement;
+      let name = productRow.querySelector('.product-title').textContent;
+      let price = Number(productRow.querySelector('.product-line-price').textContent);
+
+      output.value += `Added ${name} for ${price.toFixed(2)} to the cart.\n`;
+
+      if (!cart.hasOwnProperty(name)) {
+         cart[name] = 0;
+      }
+
+      cart[name] += price;
+   }
+
+   function onCheckout() {
+      let sum = 0;
+      for (let price of Object.values(cart)) {
+         sum += price;
+      }
+
+      let result = `You bought ${Object.keys(cart).join(', ')} for ${sum.toFixed(2)}.`;
+      output.value += result + '\n';
+
+      //Disable buttons after checkout
+      checkOutBtn.disabled = true;
+      for (let btn of buyBtns) {
+         btn.disabled = true;
+      }
+   }
+}
+
+
+
+
+
+
+
+
+
