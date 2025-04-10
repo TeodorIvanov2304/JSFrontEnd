c5e28ba5a64adfde71f2029425e3b1a4a64def35
T.Ivanov2304 <t.ivanov2304@gmail.com>
2025-04-10 14:27:57 +0300

    Create 04.Students

---
+++ 09.JS-Front-End-HTTP-and-AJAX-Exercise/09.HTTP-and-AJAX-Exercise-Resources/04.Students/app.js
@@ -0,0 +1,74 @@
+const link = 'http://localhost:3030/jsonstore/collections/students';
+document.getElementById('submit').addEventListener('click', onPost);
+
+onGet();
+
+async function onGet() {
+    let students = await getStudents();
+
+    let list = document.querySelector('tbody');
+    list.replaceChildren();
+
+    for(let student of students){
+        let row = document.createElement('tr');
+        row.innerHTML = [
+           `<td>${student.firstName}</td>
+            <td>${student.lastName}</td>
+            <td>${student.facultyNumber}</td>
+            <td>${student.grade.toFixed(2)}</td>`
+        ].join('');
+
+        list.appendChild(row);
+    }
+}
+
+async function onPost(ev) {
+    ev.preventDefault();
+
+    let firstName = document.querySelector('[name="firstName"]').value;
+    let lastName = document.querySelector('[name="lastName"]').value;
+    let facultyNumber = document.querySelector('[name="facultyNumber"]').value;
+    let grade = Number(document.querySelector('[name="grade"]').value);
+
+    if(!firstName 
+       ||!lastName
+       ||!facultyNumber
+       ||!grade){
+        return;
+    }
+
+    await postStudent(firstName, lastName, facultyNumber, grade);
+    
+    onGet();
+}
+
+//GET
+async function getStudents() {
+    let res = await fetch(link);
+
+    if(!res.ok){
+        throw new Error('Error occured');
+    }
+
+    let data = await res.json();
+
+    return Object.values(data);
+}
+
+//POST
+async function postStudent(firstName, lastName,facultyNumber,grade) {
+    let student = {
+        firstName,
+        lastName,
+        facultyNumber,
+        grade
+    };
+
+    const opitons = {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(student)
+    };
+
+    await fetch(link,opitons);
+}
